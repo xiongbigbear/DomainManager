4fd77a9215ef75284805354c18158286d41d0134
xiongbigbear <17710457586@163.com>
2018-01-16 23:18:56 +0800

    add treeview

--- DomainManager/DomainManager/MainWindow.xaml.cs
+++ DomainManager/DomainManager/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace DomainManager
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : UserControl
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
